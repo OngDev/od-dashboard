f3eb056263bf56e355f5677d5b3b2addcbe6a374
Xuan Huy <xuanhuy900@gmail.com>
2021-03-23 21:41:04 +0700

    small component

---
+++ components/SmallComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <a-card :title="title" style="width: 300px">
+      <span slot="extra" href="#"><a-icon :type="icon" /></span>
+      <a-row type="flex" justify="space-between">
+        <a-col :span="6">
+          <h2>{{ count }}</h2>
+        </a-col>
+        <a-col :span="6"> <a-icon type="arrow-up" />{{ percent }}% </a-col>
+      </a-row>
+    </a-card>
+    <br />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Page views',
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    percent: {
+      type: Number,
+      default: 0,
+    },
+    icon: {
+      type: String,
+      default: 'github',
+    },
+  },
+}
+</script>
+<style>
+.smallCard {
+  color: white;
+  background-color: blue;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,41 @@
     <div>
       <BigElement />
     </div>
+    <a-row type="flex">
+      <small-component
+        class="listCard"
+        title="Github"
+        count="30,000"
+        percent="120"
+        icon="github"
+      />
+      <small-component
+        class="listCard"
+        title="Youtube"
+        count="40,000"
+        percent="80"
+        icon="youtube"
+      />
+      <small-component
+        class="listCard"
+        title="Linked"
+        count="130,000"
+        percent="50"
+        icon="facebook"
+      />
+      <small-component
+        class="listCard"
+        title="Youtube"
+        count="10,000"
+        percent="80"
+        icon="youtube"
+      />
+    </a-row>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+export default {}
 </script>
 
 <style>
